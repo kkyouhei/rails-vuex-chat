aadfd7858c6c4a8d8bbac6dda3f8e8eb62e8cead
kkyouhei <hope637@gmail.com>
2017-12-16 04:38:17 +0900

    検索フォームをコンポーネント化して再利用性を向上

---
+++ app/javascript/components/SearchField.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" @input="changeValue" placeholder="user name"/>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  name: 'SearchField' ,
+
+  props: {
+    action: {
+      type: Function,
+      required: false,
+    }
+  },
+
+  methods: {
+    changeValue(val){
+      this.action(val.target.value)
+    }
+  }
+}
+</script>
--- app/javascript/components/UserSearch.vue
+++ app/javascript/components/UserSearch.vue
@@ -1,17 +1,23 @@
 <template>
-  <input type="text" @input="searchUser" placeholder="user name"/>
+  <div>
+    <SearchField :action="searchUser" />
+  </div>
 </template>
 
 <script>
 import {mapActions} from 'vuex'
+import SearchField from './SearchField.vue'
 
 export default {
+  components: {
+    SearchField
+  },
   methods: {
     ...mapActions([
       'search'
     ]),
     searchUser(val){
-      this.search(val.target.value)
+      this.search(val)
     }
   }
 }
